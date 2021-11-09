5bf0f87802467ff779605093317583e3513e970a
Amolo <railamolo@gmail.com>
2021-11-09 09:50:10 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Button>Click Me</Button>
+    </Container>
+  );
+}
